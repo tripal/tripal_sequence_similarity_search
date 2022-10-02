60f3a366e24c835546df407fc7cd5c7d5ca91f3b
Ferrisx4 <ferrisx4@gmail.com>
2022-10-02 17:24:37 -0400

    Make the functions classy

--- src/Controller/Tripal_SeqAdminUtilities.php
+++ src/Controller/Tripal_SeqAdminUtilities.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Drupal\tripal_seq\Controller;
 /**
  * This file will include all functions related to handling
  * the database list, categories, and, and other useful utilities.
@@ -7,24 +8,27 @@
  * have their own place in the Form area (../Form/)
  */
 
-/**
- * Handle import/export of the Diamond and BLAST database list.
- */
-function tripal_seq_db_import() {
 
-}
-
-function tripal_seq_db_export() {
-
-}
-
-/**
- * Handle the import/export of the Categories list
- */
-function tripal_seq_categories_import() {
+class Tripal_SeqAdminUtilities {
+    /**
+     * Handle import/export of the Diamond and BLAST database list.
+     */
+    function tripal_seq_db_import() {
     
-}
-
-function tripal_seq_categories_export() {
-
+    }
+    
+    function tripal_seq_db_export() {
+    
+    }
+    
+    /**
+     * Handle the import/export of the Categories list
+     */
+    function tripal_seq_categories_import() {
+    
+    }
+    
+    function tripal_seq_categories_export() {
+    
+    }
 }
